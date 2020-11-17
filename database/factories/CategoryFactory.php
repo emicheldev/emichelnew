ebe1d90d706fbf894f94d11d73380b8ffb0c37f0
emichel <emicheldev@gmail.com>
2020-11-17 18:56:05 +0100

    Costome Category table

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,6 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Models\Category::class, function (Faker $faker) {
       return [
        'name' => $faker->unique()->word,
+       'slug' => $faker->slug,
        'icon' => $faker->unique()->word,
        'color' => $faker->safeColorName,
        'bgcolor' => $faker->hexColor,
